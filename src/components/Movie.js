6b3d53891564cebd5dd80df6e4afb4bbf473cbeb
Nihal <nihalkudukkan@gmail.com>
2022-07-17 23:43:03 +0530

    page design start

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import './movie.css'
+import thallumala from "../asset/thallumala.jpg"
 
 export default function Movie() {
   return (
-    <div>Movie</div>
+    <div className='movie__page'>
+        <div className='container'>
+            <div className='img__box'>
+                <img src={thallumala} alt="" />
+            </div>
+        </div>
+    </div>
   )
 }
