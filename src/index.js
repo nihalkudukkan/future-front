0299d946bdb7adbb9e7f6fbc8b6d14b1d5705d90
Nihal <nihalkudukkan@gmail.com>
2022-07-17 23:30:21 +0530

    movie page creation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Movie from './components/Movie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} exact/>
+        <Route path="/movie" element={<Movie />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
